81f7198d9271084677599c7d5286346d592b8816
Mahmoud Reda <sim.mahmoud.reda@gmail.com>
2024-11-29 00:58:54 +0200

    initial Commit & Project Structure🙏🏻

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/signin",
+        permanent: true,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/signin",
+        destination: "/auth/signin/page.tsx", // Map /signin to auth/signin
+      },
+      {
+        source: "/signup",
+        destination: "/auth/signup/", // Map /signup to auth/signup
+      },
+    ];
+  },
 };
 
 export default nextConfig;
